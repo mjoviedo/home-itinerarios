caa99ef7301d15bfe9c6e03bdcd10884848d0dd1
moviedo <moviedo@unc.edu.ar>
2026-04-17 16:24:38 -0300

    Nuevo mapa

--- map.js
+++ map.js
@@ -10,8 +10,8 @@ var map = L.map('map', {
 });
 
 
-var bounds = [[200,200], [4000,8000]];
-var image = L.imageOverlay('img/mapa-cba.png', bounds).addTo(map);
+var bounds = [[0,0], [6214,4163]];
+var image = L.imageOverlay('img/1888-recorte-mapa-comprimido.jpg', bounds).addTo(map);
 
 map.fitBounds(bounds);
 
@@ -25,26 +25,27 @@ var pin = L.icon({
 });
 
 
-var cruzdeleje = L.marker([3700, 3080],{icon: pin}).addTo(map);
-cruzdeleje.bindPopup("<h1>1917</h1><br><em>Escuela Normal Superior República del Perú</em><br>Cruz del Eje");
 
-var sanfrancisco = L.marker([2760, 6380],{icon: pin}).addTo(map);
-sanfrancisco.bindPopup("<h1>1912</h1><br><em>Escuela Normal Nacional</em><br>San Francisco");
+var alberdi = L.marker([3500, 1740],{icon: pin}).addTo(map);
+alberdi.bindPopup("<h1>1906</h1><img src='img/escuelas-normales/alberdi.jpg'><br><em>Escuela Graduada Superior Normal Provincial de Niñas Juan Bautista Alberdi</em><br>Córdoba Capital");
 
-var alberdi = L.marker([2760, 3710],{icon: pin}).addTo(map);
-alberdi.bindPopup("<h1>1906</h1><br><em>Escuela Graduada Superior Normal Provincial de Niñas Juan Bautista Alberdi</em><br>Córdoba Capital");
+var maestras = L.marker([3470, 1695],{icon: pin}).addTo(map);
+maestras.bindPopup("<h1>1884</h1><img src='img/escuelas-normales/carbo.jpg'><br><em>Escuela Normal Nacional de Maestras </em><br>Córdoba Capital");
 
-var maestras = L.marker([2820, 3810],{icon: pin}).addTo(map);
-maestras.bindPopup("<h1>1884</h1><br><em>Escuela Normal Nacional de Maestras </em><br>Córdoba Capital");
+var olmos = L.marker([3500, 1650],{icon: pin}).addTo(map);
+olmos.bindPopup("<h1>1909</h1><img src='img/escuelas-normales/olmos.jpg'><br><em>Escuela Normal Provincial para Varones Jose Vicente de Olmos </em><br>Córdoba Capital");
 
-var olmos = L.marker([2730, 3910],{icon: pin}).addTo(map);
-olmos.bindPopup("<h1>1909</h1><br><em>Escuela Normal Provincial para Varones Jose Vicente de Olmos </em><br>Córdoba Capital");
+var cruzdeleje = L.marker([3955, 1283],{icon: pin}).addTo(map);
+cruzdeleje.bindPopup("<h1>1917</h1><img src='img/escuelas-normales/cruz-del-eje.jpg'><br><em>Escuela Normal Superior República del Perú</em><br>Cruz del Eje");
 
-var riocuarto = L.marker([500, 3680],{icon: pin}).addTo(map);
-riocuarto.bindPopup("<h1>1909</h1><br><em>Escuela Normal Nacional Mixta</em><br>Río Cuarto");
+var sanfrancisco = L.marker([3450, 3000],{icon: pin}).addTo(map);
+sanfrancisco.bindPopup("<h1>1912</h1><img src='img/escuelas-normales/san-francisco.jpg'><br><em>Escuela Normal Nacional</em><br>San Francisco");
 
-var bellville = L.marker([1200, 5650],{icon: pin}).addTo(map);
-bellville.bindPopup("<h1>1909</h1><br><em>Escuela Normal Nacional Mixta</em><br>Bell Ville");
+var riocuarto = L.marker([2220, 1580],{icon: pin}).addTo(map);
+riocuarto.bindPopup("<h1>1909</h1><img src='img/escuelas-normales/rio-cuarto.jpg'><br><em>Escuela Normal Nacional Mixta</em><br>Río Cuarto");
 
-var villadolores = L.marker([2100, 2600],{icon: pin}).addTo(map);
-villadolores.bindPopup("<h1>1910</h1><br><em>Escuela Normal Rural Mixta</em><br>Villa Dolores");
+var bellville = L.marker([2580, 2630],{icon: pin}).addTo(map);
+bellville.bindPopup("<h1>1909</h1><img src='img/escuelas-normales/bell-ville.jpg'><br><em>Escuela Normal Nacional Mixta</em><br>Bell Ville");
+
+var villadolores = L.marker([3100, 1000],{icon: pin}).addTo(map);
+villadolores.bindPopup("<h1>1910</h1><img src='img/escuelas-normales/villa-dolores.jpg'><br><em>Escuela Normal Rural Mixta</em><br>Villa Dolores");
